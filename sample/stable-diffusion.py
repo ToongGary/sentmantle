19d3c9dcf07ed14e05f07bbc03fa7605790f463e
LeeShinYeoung <iqrash12@gmail.com>
2023-07-14 01:14:34 +0900

    Fix stable diffusion error

--- sample/stable-diffusion.py
+++ sample/stable-diffusion.py
@@ -3,11 +3,13 @@ import torch
 
 model_id = "stabilityai/stable-diffusion-2-1-base"
 
-scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+scheduler = EulerDiscreteScheduler.from_pretrained(
+    model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_id, scheduler=scheduler, torch_dtype=torch.float32)
+pipe = pipe.to("mps" if torch.backends.mps.is_available() else "cpu")
 
 prompt = "a photo of an astronaut riding a horse on mars"
-image = pipe(prompt).images[0]  
-    
+image = pipe(prompt).images[0]
+
 image.save("astronaut_rides_horse.png")
